8b3322482293a777d577d91a72efe8bb493e9f91
Jack <mysamsung22022014@gmail.com>
2022-11-11 22:37:08 +0300

    Update GroupChanger.lua

--- Scripts/GMStuff/GroupChanger.lua
+++ Scripts/GMStuff/GroupChanger.lua
@@ -1,3 +1,9 @@
+--A020F0 fiol
+--DA1A18 red
+--FFFFFF white
+--E7E52C ye
+--31B32B gre
+colors = {Purple = "A020F0" , Red = "DA1A18", White = "FFFFFF", Yellow = "E7E52C", Green = "31B32B"}
 
 
 function onLoad()
@@ -11,6 +17,7 @@ function onLoad()
         color          = {0,0,0},
         font_color     = {1,1,1},
         font_size      = 600,
+        tooltip        = "Текущая группа: ".. self.getGMNotes()
     })
     changeGroup()
     changeGroup()
@@ -46,5 +53,32 @@ function changeGroup()
                 obj.setInvisibleTo()
             end
         end
+    end
+    self.editButton({index = 0, tooltip = "Текущая группа: ".. self.getGMNotes()})
+    changeNotes()
+end
+
+function changeNotes()
+    notes = {}
+    for i, note in pairs(getNotebookTabs()) do
+        for color_name, color_code in pairs(colors) do
+            if string.find(note.title, ".+" ..  color_code .. ".+") ~= nil then
+                if note.color == "Black" then
+                    note.color = tostring(color_name)
+                else
+                    note.color = "Black"
+                end
+                break
+            end
+        end
+        notes[i] = note
+    end
+
+    for i = #notes, 1, -1 do
+        removeNotebookTab(i-1)
+    end
+    
+    for i = 1, #notes do
+        addNotebookTab(notes[i])
     end
 end
